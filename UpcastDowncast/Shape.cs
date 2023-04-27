6b5d3855f29029c0d2eea746eee0dbaf18b19be4
jeffersonong <erson83@hotmail.com>
2023-04-27 18:00:31 +0800

    upcastdowncast

---
+++ UpcastDowncast/Shape.cs
@@ -0,0 +1,24 @@
+﻿namespace UpcastDowncast
+
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw()
+        {
+
+
+        }
+
+
+    }
+
+
+
+
+}
+
---
+++ UpcastDowncast/Text2.cs
@@ -0,0 +1,18 @@
+﻿namespace UpcastDowncast
+
+{
+    public class Text2 : Shape2
+
+    // text is child class, shape is parent class
+    // child will inherit parent class
+    {
+        public int Fontsize { get; set; }
+        public string FontName { get; set; }
+
+    }
+
+
+
+
+}
+
