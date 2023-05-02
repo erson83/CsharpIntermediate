2d1e47d2919c0ae778f3f2484087e6dbeea4d1c5
jeffersonong <erson83@hotmail.com>
2023-05-02 18:05:37 +0800

    push

--- AbstractClassesMembers/Program.cs
+++ AbstractClassesMembers/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace AbstractClasses
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            // remember this is different if we use abstract
+            // we cannot instatiate Shape
+            // we cannot - var shape = new Shape();
+
+
+            var circle = new Circle();
+            circle.Draw();
+
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+
+            rectangle.Copy();
+
+
+        }
+
+    }
+
+
+}
---
+++ AbstractClassesMembers/Shape.cs
@@ -0,0 +1,24 @@
+﻿namespace AbstractClasses
+
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+        public abstract void Draw();
+        
+            // cannot define this if using Abstract class
+            // derived class will take control eg. Rectangle : Shape
+
+
+        public void Copy()
+        {
+            // this will be the main class
+            // we don need the derived class to take control
+            Console.WriteLine("Copy shape into clipboard");
+        }
+    }
+
+}
