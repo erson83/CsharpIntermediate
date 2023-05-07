554b988d248e5998a5554fbe3d902a6e84b379cd
jeffersonong <erson83@hotmail.com>
2023-05-07 19:51:19 +0800

    push from imac

---
+++ ExcerciseDBCommand/DBConnection.cs
@@ -0,0 +1,41 @@
+﻿namespace ExDBCommand
+
+
+
+{
+
+    public class DBConnection : DBCommand
+    {
+        public DBConnection(string connectionstring, string sqlquery, string sqltype)
+        : base(connectionstring, sqlquery, sqltype)
+        {
+
+        }
+
+
+        public override void Open()
+        {
+            if (SqlType == "SQL")
+            Console.WriteLine("Opening SQL connection: " + ConnectionString);
+            else
+            Console.WriteLine("Opening Oracle connection: " + ConnectionString);
+        }
+
+        public override void Run()
+        {
+                Console.WriteLine("Running connection string:  " + SqlQuery);
+        }
+
+        public override void Close()
+        {
+            if (SqlType == "SQL")
+                Console.WriteLine("Closing SQL connection.  ");
+            else
+                Console.WriteLine("Closing Oracle connection.  ");
+        }
+
+    }
+
+
+
+}
